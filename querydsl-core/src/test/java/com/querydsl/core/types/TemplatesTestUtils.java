5820585c687643c722fb47b138a121a887410866
Timo Westkämper <timo.westkamper@gmail.com>
2015-03-04 23:30:40 +0200

    Tune precedence mappings

--- querydsl-core/src/test/java/com/querydsl/core/types/TemplatesTestUtils.java
+++ querydsl-core/src/test/java/com/querydsl/core/types/TemplatesTestUtils.java
@@ -15,14 +15,14 @@ public final class TemplatesTestUtils {
             Template template = templates.getTemplate(op);
             String str = template.toString();
             int precedence = templates.getPrecedence(op);
-            if (str.matches("\\w+\\([^\\(\\)]+\\)") && precedence > -1) {
-                Assert.fail("Unexpected precedence for " + op + " with template " + template);
-            } else if (str.contains(" like ") && precedence != likePrecedence) {
+            if (str.contains(" like ") && precedence != likePrecedence) {
                 Assert.fail("Unexpected precedence for " + op + " with template " + template);
             } else if (!str.contains("(") && precedence < 0) {
                 Assert.fail("Unexpected precedence for " + op + " with template " + template);
             } else if (str.endsWith(" + 1") || str.endsWith("+1") || str.endsWith(" - 1") || str.endsWith("-1")) {
-                Assert.fail("Unsafe pattern for " + op + " with template " + template);
+                if (precedence != Templates.Precedence.ARITH_LOW) {
+                    Assert.fail("Unsafe pattern for " + op + " with template " + template);
+                }
             }
         }
     }
